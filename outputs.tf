c6d445090725162f9ec05c81b7e98896626bf35f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "log_analytics_linked_storage_accounts" {
-  description = "All log_analytics_linked_storage_account resources"
-  value       = azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts
-}
 output "log_analytics_linked_storage_accounts_data_source_type" {
-  description = "List of data_source_type values across all log_analytics_linked_storage_accounts"
-  value       = [for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : v.data_source_type]
+  description = "Map of data_source_type values across all log_analytics_linked_storage_accounts, keyed the same as var.log_analytics_linked_storage_accounts"
+  value       = { for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : k => v.data_source_type }
 }
 output "log_analytics_linked_storage_accounts_resource_group_name" {
-  description = "List of resource_group_name values across all log_analytics_linked_storage_accounts"
-  value       = [for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : v.resource_group_name]
+  description = "Map of resource_group_name values across all log_analytics_linked_storage_accounts, keyed the same as var.log_analytics_linked_storage_accounts"
+  value       = { for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : k => v.resource_group_name }
 }
 output "log_analytics_linked_storage_accounts_storage_account_ids" {
-  description = "List of storage_account_ids values across all log_analytics_linked_storage_accounts"
-  value       = [for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : v.storage_account_ids]
+  description = "Map of storage_account_ids values across all log_analytics_linked_storage_accounts, keyed the same as var.log_analytics_linked_storage_accounts"
+  value       = { for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : k => v.storage_account_ids }
 }
 output "log_analytics_linked_storage_accounts_workspace_id" {
-  description = "List of workspace_id values across all log_analytics_linked_storage_accounts"
-  value       = [for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : v.workspace_id]
+  description = "Map of workspace_id values across all log_analytics_linked_storage_accounts, keyed the same as var.log_analytics_linked_storage_accounts"
+  value       = { for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : k => v.workspace_id }
 }
 output "log_analytics_linked_storage_accounts_workspace_resource_id" {
-  description = "List of workspace_resource_id values across all log_analytics_linked_storage_accounts"
-  value       = [for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : v.workspace_resource_id]
+  description = "Map of workspace_resource_id values across all log_analytics_linked_storage_accounts, keyed the same as var.log_analytics_linked_storage_accounts"
+  value       = { for k, v in azurerm_log_analytics_linked_storage_account.log_analytics_linked_storage_accounts : k => v.workspace_resource_id }
 }
 
